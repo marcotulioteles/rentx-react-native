421c42d0d049a02ef503b8ed9c10cd1ed2dad835
marcotulioteles <marcotuliocivileng@gmail.com>
2021-08-17 06:43:57 -0300

    created Card Car Component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -12,6 +13,25 @@ import {
 } from './styles';
 
 export function Home() {
+  const carData1 = {
+    brand: 'audi',
+    name: 'rs 5 coupe',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png',
+  }
+  const carData2 = {
+    brand: 'porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://purepng.com/public/uploads/large/purepng.com-black-porsche-panamera-carcarvehicletransportporsche-961524660080ezwd4.png',
+  }
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +50,13 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car 
+        data = {carData1}
+      />
+      <Car 
+        data = {carData2}
+      />
     </Container>
   );
 }
