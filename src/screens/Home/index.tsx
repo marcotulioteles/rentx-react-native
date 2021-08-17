9e75f304de0d7cd976b439671b4907a758a83963
marcotulioteles <marcotuliocivileng@gmail.com>
2021-08-17 05:52:02 -0300

    created header component and styles

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import {
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+} from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+}
