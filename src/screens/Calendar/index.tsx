fd2bac6d9c24a5323786b022c3f4487acedb254c
MarcoTulioTeles <marcotuliocivileng@gmail.com>
2021-08-21 20:35:27 -0300

    created Calendar Component

---
+++ src/screens/Calendar/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons'
+
+import { 
+  Calendar as CustomCalendar, 
+  LocaleConfig
+} from 'react-native-calendars';
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+     'Março',
+     'Abril',
+     'Maio',
+     'Junho',
+     'Julho',
+     'Agosto',
+     'Setembro',
+     'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+  monthNamesShort: [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
+  today: 'Hoje'
+}
+
+LocaleConfig.defaultLocale = 'pt-br';
+
+import {
+  Container,
+} from './styles'
+import { useTheme } from 'styled-components';
+
+export function Calendar() {
+  const theme = useTheme()
+
+  return (
+    <CustomCalendar 
+      renderArrow={( direction ) =>
+        <Feather 
+          size={24}
+          color={theme.colors.text}
+          name={direction == 'left' ? "chevron-left" : "chevron-right"}
+        />
+      }
+
+      headerStyle={{
+        backgroundColor: theme.colors.background_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_detail,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        textDayHeaderFontSize: 10,
+        textMonthFontSize: 20,
+        monthTextColor: theme.colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        }
+      }}
+
+      firstDay={1}
+      minDate={new Date()}
+    />
+  )
+}
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
 import { useTheme } from 'styled-components';
 import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg'
 
 import {
   Container,
   Header,
-  Title
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
 } from './styles';
+import { Button } from '../../components/Button';
+import { Calendar } from '../Calendar';
 
 export function Scheduling() {
   const theme = useTheme();
@@ -14,15 +26,42 @@ export function Scheduling() {
   return (
     <Container>
       <Header>
-        <BackButton 
-          onPress={() => {}}
-          color={theme.colors.shape}  
+        <StatusBar 
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
         />
-      
+        <BackButton
+          onPress={() => { }}
+          color={theme.colors.shape}
+        />
+
         <Title>
           Escolha uma {'\n'}data de início e {'\n'}fim do aluguel
         </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}></DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}></DateValue>
+          </DateInfo>
+        </RentalPeriod>
       </Header>
+
+      <Content>
+        <Calendar />
+      </Content>      
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
