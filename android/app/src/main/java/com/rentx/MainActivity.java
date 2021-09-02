1cc517023361fdfb9a8763359bd1754cbde45ce3
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-09-02 20:12:03 -0300

    fix bug gradle version replacement android folder

--- android/app/src/main/java/com/rentx/MainActivity.java
+++ android/app/src/main/java/com/rentx/MainActivity.java
@@ -42,9 +42,4 @@ public class MainActivity extends ReactActivity {
             }
         };
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
 }
