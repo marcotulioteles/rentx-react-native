91bc57cbcb116d3f907b518d89384e37399e6aaf
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-09-06 18:21:07 -0300

    updating profile data from the client side

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -29,6 +29,7 @@ interface AuthContextData {
   user: User;
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
+  updatedUser: (user: User) => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -82,6 +83,25 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function updatedUser(user: User) {
+    try {
+      const userCollection = database.get<ModelUser>('users');
+      await database.write(async () => {
+        const userSelected = await userCollection.find(user.id);
+        await userSelected.update(( userData ) => {
+          userData.name = user.name,
+          userData.driver_license = user.driver_license,
+          userData.avatar = user.avatar
+        });
+      });
+
+      setData(user);
+
+    } catch (error: any) {
+      throw new Error(error)
+    }
+  }
+
   useEffect(() => {
     async function loadUserData() {
       const userCollection = database.get<ModelUser>('users');
@@ -100,7 +120,8 @@ function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider value={{
       user: data,
       signIn,
-      signOut
+      signOut,
+      updatedUser
     }}>
       {children}
     </AuthContext.Provider>
