940c8a2311b9e4c1a2c94759b8700ba200c42c85
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-09-09 18:01:15 -0300

    created carSchema and carModel for WatermelonDB

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,7 @@ import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
 
 import { schemas } from './schema';
 import { User } from './model/User';
+import { Car } from './model/Car';  
 
 const adapter = new SQLiteAdapter({
   schema: schemas
@@ -10,5 +11,8 @@ const adapter = new SQLiteAdapter({
 
 export const database = new Database({
   adapter,
-  modelClasses: [User],
+  modelClasses: [
+    User,
+    Car
+  ],
 });
