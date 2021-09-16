1efe58c9a9f199a69c82ba49c2da682d91d013b8
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-09-16 16:56:22 -0300

    synchronize user when return from offline-online

---
+++ src/@types/navigate-from-react-navigate/index.ts
@@ -0,0 +1,4 @@
+export interface NavigationProps {
+  navigate: (screen: string) => void;
+  goBack: () => void;
+}
--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { RectButtonProps } from 'react-native-gesture-handler';
-import GasolineSvg from '../../assets/gasoline.svg';
-import { CarDTO } from '../../dtos/CarDTO';
 import { Car as ModelCar } from '../../database/model/Car';
 import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
@@ -21,7 +19,7 @@ import {
 } from './styles';
 
 interface Props extends RectButtonProps {
-  data: CarDTO;
+  data: ModelCar;
 }
 
 export function Car({ data, ...rest }: Props) {
