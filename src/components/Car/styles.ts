bdc2099512abb8814bbdc35b907f6f8cbe3efbbd
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-09-16 17:24:07 -0300

    using r-n-fast-image to cache images after load

--- src/components/Car/styles.ts
+++ src/components/Car/styles.ts
@@ -1,6 +1,7 @@
 import { RectButton } from 'react-native-gesture-handler';
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import FastImage from 'react-native-fast-image';
 
 export const Container = styled(RectButton)`
   width: 100%;
@@ -62,7 +63,7 @@ export const Price = styled.Text`
 export const Type = styled.View`
 `;
 
-export const CarImage = styled.Image`
+export const CarImage = styled(FastImage)`
   width: 167px;
   height: 85px;
 `;
